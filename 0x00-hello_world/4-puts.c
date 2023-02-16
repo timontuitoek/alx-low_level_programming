6cf6e2515f301ddfe6074c3797137d1a1303d5ad
timontuitoek <tuitoektimon@gmail.com>
2023-02-16 13:46:38 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
 int main(void);
-{	puts("programming is like building a multilingual puzzle");
+{	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
