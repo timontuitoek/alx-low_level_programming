7d1a995f177a4bcf0b3e5047890372c70673f266
timontuitoek <tuitoektimon@gmail.com>
2023-02-16 13:16:14 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
-#!/bin/bash
-/*
- * programming is like building a multilingual puzzle*/
 #include <studio.h>
 int main(void);
-{puts("programming is like building a multilingual puzzle");
-return(0);
+{	puts("programming is like building a multilingual puzzle");
+	return (0);
 }
 
