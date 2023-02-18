051c47f94274ce513ca13086a3afb9d9de646095
timontuitoek <tuitoektimon@gmail.com>
2023-02-18 12:56:11 -0800

    Combination of brains and material

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,20 +3,29 @@
  * main - main function
  *
  * Return: 0 always
+ *
  */
+
 int main(void)
 {
 	int i;
+	int j;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
+		for (j = 1 ; j < 10 ; j++)
+
 		putchar(i + '0');
 		if (i != 9)
 		{
 			putchar(',');
-			putchar(' ');
+			putchar('0');
+			if (1 + j != 17)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
-
 	}
 	putchar('\n');
 	return (0);
