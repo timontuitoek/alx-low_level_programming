8f69dc604e819ab565b096043930f351ff3d9d58
timontuitoek <tuitoektimon@gmail.com>
2023-02-20 12:37:52 -0800

    Success combination in business

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,24 +8,22 @@
 
 int main(void)
 {
-	int i;
-	int j;
+	int a, b;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (a = '0'; a < '9'; a++)
 	{
-		for (j = 1 ; j < 10 ; j++)
-
-		putchar(i + '0');
-		if (i != 9)
-		{
-			putchar(',');
-			putchar('0');
-			if (1 + j != 17)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
+	for (b = a + 1; b <= '9'; b++)
+	{
+	if (b != a)
+{
+	      putchar(a);
+	      putchar(b);
+	if (a == '8' && b == '9')
+	continue;
+	 putchar(',');
+	 putchar(' ');
+}
+	}
 	}
 	putchar('\n');
 	return (0);
