bfa46f8df6dffd3267222cbda5d62f1926e6affb
timontuitoek <tuitoektimon@gmail.com>
2023-02-21 05:57:03 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+ *Main -  entry point
+ *
+ *Description: prints the entry point
+ *
+ *Return: 0 successfully
+ */
+
+int main(void)
+{
+        putchar(' ');
+        putchar('p');
+        putchar('u');
+        putchar('t');
+        putchar('c');
+        putchar('h');
+        putchar('a');
+        putchar('r');
+        putchar('\n');
+        return (0);
+}
