e13ea1cc218957cc38628d8380567beaf091fdea
timontuitoek <tuitoektimon@gmail.com>
2023-02-23 07:11:19 -0800

    Isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks the code
+ *
+ *@c: integer to check code
+ *
+ * Return: 0 always
+ */
+int _isupper(int c)
+{
+	c = 0;
+
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
