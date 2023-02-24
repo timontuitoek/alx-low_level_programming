e7faf6f40f719c7be7a2568c79072d4fafa2e875
timontuitoek <tuitoektimon@gmail.com>
2023-02-24 00:59:59 -0800

    Multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,12 +3,24 @@
 /**
  * mul - multiplication factor
  * @a: the first integer
- * @b - the second integer
+ * @b: the second integer
  *
  * Return: 0 successful
  */
 
+int mul(int a, int b);
+
+/**
+ * mul - function definition
+ * Description: multiply 2 numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: the product
+ */
+
 int mul(int a, int b)
 {
-	return (a * b);
+	int product = a * b;
+
+	return (product);
 }
