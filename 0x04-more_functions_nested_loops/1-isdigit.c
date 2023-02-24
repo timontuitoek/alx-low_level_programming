fe59fda587bf60b8bf1d35a5fd03383565bc047d
timontuitoek <tuitoektimon@gmail.com>
2023-02-24 01:02:22 -0800

    Is_digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,11 +8,24 @@
  * Return: 1 if is between range else 0
  */
 
+int _isdigit(int c);
+
+/**
+ * _isdigit - function definition
+ * Description: test if c is a digit
+ * @c: the value to test
+ * Return: 1-true, 0-false
+ */
+
 int _isdigit(int c)
 {
-
-	if ((c <= '0' && c <= '9'))
+	if (c >= '0' && c <= '9')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
+
