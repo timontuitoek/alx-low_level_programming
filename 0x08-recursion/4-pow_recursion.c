bc7f083cb0b8786d2bff36649638467f95ad8da4
timontuitoek <tuitoektimon@gmail.com>
2023-03-08 11:43:01 -0800

    Functions returning value of x raised to power y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *_pow_recursion - pow recursion
+ *@x: first integer
+ *@y: second integer
+ * Return: 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
