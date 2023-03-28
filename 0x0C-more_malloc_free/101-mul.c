08ca0dd6cadf74156a37af4c926d0581d554cbb5
timontuitoek <tuitoektimon@gmail.com>
2023-03-28 01:34:49 -0700

    A program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,45 +11,45 @@
  */
 int multiply(const char *num1, const char *num2)
 {
-    int len1 = 0, len2 = 0, i, j, res_len, carry = 0, sum = 0;
-    int *result;
+	int len1 = 0, len2 = 0, i, j, res_len, carry = 0, sum = 0;
+	int *result;
 
-    while (num1[len1])
-        len1++;
-    while (num2[len2])
-        len2++;
+	while (num1[len1])
+	len1++;
+	while (num2[len2])
+	len2++;
 
-    res_len = len1 + len2;
-    result = calloc(res_len, sizeof(int));
+	res_len = len1 + len2;
+	result = calloc(res_len, sizeof(int));
 
-    if (!result)
-        exit(98);
+	if (!result)
+	exit(98);
 
-    for (i = len1 - 1; i >= 0; i--)
-    {
-        carry = 0;
-        for (j = len2 - 1; j >= 0; j--)
-        {
-            sum = (num1[i] - '0') * (num2[j] - '0') +
-                  result[i + j + 1] + carry;
-            carry = sum / 10;
-            result[i + j + 1] = sum % 10;
-        }
-        result[i + j + 1] = carry;
-    }
+	for (i = len1 - 1; i >= 0; i--)
+	{
+	carry = 0;
+	for (j = len2 - 1; j >= 0; j--)
+	{
+		sum = (num1[i] - '0') * (num2[j] - '0') +
+		result[i + j + 1] + carry;
+		carry = sum / 10;
+		result[i + j + 1] = sum % 10;
+	}
+	result[i + j + 1] = carry;
+	}
 
-    i = 0;
-    while (result[i] == 0 && i < res_len - 1)
-        i++;
+	i = 0;
+	while (result[i] == 0 && i < res_len - 1)
+	i++;
 
-    for (; i < res_len; i++)
-        printf("%d", result[i]);
+	for (; i < res_len; i++)
+	printf("%d", result[i]);
 
-    printf("\n");
+	printf("\n");
 
-    free(result);
+	free(result);
 
-    return (0);
+	return (0);
 }
 
 /**
@@ -61,29 +61,30 @@ int multiply(const char *num1, const char *num2)
  */
 int main(int argc, char **argv)
 {
-    int i;
+	int i;
 
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return (98);
-    }
+	if (argc != 3)
+	{
+	printf("Error\n");
+	return (98);
+	}
 
-    for (i = 1; i < argc; i++)
-    {
-        int j = 0;
-        while (argv[i][j])
-        {
-            if (!isdigit(argv[i][j]))
-            {
-                printf("Error\n");
-                return (98);
-            }
-            j++;
-        }
-    }
+	for (i = 1; i < argc; i++)
+	{
+	int j = 0;
 
-    multiply(argv[1], argv[2]);
+	while (argv[i][j])
+	{
+		if (!isdigit(argv[i][j]))
+		{
+		printf("Error\n");
+		return (98);
+		}
+		j++;
+	}
+	}
 
-    return (0);
+	multiply(argv[1], argv[2]);
+
+	return (0);
 }
