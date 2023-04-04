04b5c2d5cf7b11c9d60d90ef93530094f2244c21
timontuitoek <tuitoektimon@gmail.com>
2023-04-04 05:09:40 -0700

    A function that frees a listint list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - free's a listint_t list
+ * @head: pointer to head of list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current_node;
+
+	if (head == NULL)
+
+
+	while (*head != NULL)
+	{
+		current_node = *head;
+		*head = (*head)->next;
+		free(current_node);
+	}
+	*head = NULL;
+}
+
