492c113191c3b723f18eb15069beb2a8ec43d8e9
timontuitoek <tuitoektimon@gmail.com>
2023-05-04 10:59:01 -0700

    Header file

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifdef MAAIN_H
+#define MAIN_H
+
+int main(void);
+int _putchar(char_c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+print_binary();
+
+#endif
