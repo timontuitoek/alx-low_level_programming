c5f21f765d5b8b5804cb3f698ecae127b666036c
timontuitoek <tuitoektimon@gmail.com>
2023-05-09 01:25:01 -0700

    A function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,24 +11,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file_description;
+	int f_descriptor;
 	ssize_t bytes_written;
 
 	if (filename == NULL)
 		return (-1);
-	file_description = popen(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (file_description == -1)
+	f_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (f_descriptor == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
-	bytes_written = write(file_description, text_content, strlen(text_content));
+	bytes_written = write(f_descriptor, text_content, strlen(text_content));
 	if (bytes_written == -1)
 	{
-		pclose(file_description);
+		close(f_descriptor);
 		return (-1);
 	}
 	}
-	pclose(file_description);
+	close(f_descriptor);
 	return (1);
 }
 
