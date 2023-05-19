0e89466e0135ab0ada9d0cc4faf5cf299c762e8d
timontuitoek <tuitoektimon@gmail.com>
2023-05-19 02:32:54 -0700

    A program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,91 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-#define BUF_SIZE 1024
 
-int copy_file(char *file_from, char *file_to);
-void print_usage(char *program_name);
-int main(int argc, char *argv[]);
+#define MAXSIZE 1024
+#define SE STDERR_FILENO
 
 /**
- * main - copies the contents of a file to another file.
- * @argc: argument counts
- * @argv: argument vectors
- * Return: 0 on success.
+ * main - create copy bash script
+ * @argc: argument count
+ * @argv: arguments string
+ *
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
+	int input_fdescriptor, output_fdescriptor, istatus, ostatus;
+	char buf[MAXSIZE];
+	mode_t mode;
+
+	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	if (argc != 3)
 	{
-		print_usage(argv[0]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
-	return (copy_file(argv[1], argv[2]));
-	return (0);
-}
-
-/**
- * print_usage - print usage message and exits with code 97
- * @program_name: name of the program
- * Return: none
- */
-
-void print_usage(char *program_name)
-{
-	dprintf(STDERR_FILENO, "Usage: cp %s file_from file_to\n", program_name);
-	exit(97);
-}
-
-/**
- * copy_file - copies content of one file to another
- * @file_from: source file
- * @file_to: destination file
- * Return: 0 on success,  exit otherwise
- */
-
-int copy_file(char *file_from, char *file_to)
-{
-	int filed_from, filed_to, bytes_read, bytes_written, close_status;
-	char buf[BUF_SIZE];
-
-	filed_from = open(file_from, O_RDONLY);
-	if (filed_from == -1)
+	input_fdescriptor = open(argv[1], O_RDONLY);
+	if (input_fdescriptor == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	filed_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (filed_to == -1)
+	output_fdescriptor = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (output_fdescriptor == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	while ((bytes_read = read(filed_from, buf, BUF_SIZE)) > 0)
-	{
-		bytes_written = write(filed_to, buf, bytes_read);
-		if (bytes_written == -1)
+	do {
+		istatus = read(input_fdescriptor, buf, MAXSIZE);
+		if (istatus == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
-			exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
 		}
-	}
-	if (bytes_read == -1)
+		if (istatus > 0)
+		{
+			ostatus = write(output_fdescriptor, buf, (ssize_t) istatus);
+			if (ostatus == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+				exit(99);
+			}
+		}
+	} while (istatus > 0);
+	istatus = close(input_fdescriptor);
+	if (istatus == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-	close_status = close(filed_from);
-	if (close_status == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close filed %d\n", filed_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", input_fdescriptor);
 		exit(100);
 	}
-	close_status = close(filed_to);
-	if (close_status == -1)
+	ostatus = close(output_fdescriptor);
+	if (ostatus == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", filed_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", output_fdescriptor);
 		exit(100);
 	}
 	return (0);
 }
-
