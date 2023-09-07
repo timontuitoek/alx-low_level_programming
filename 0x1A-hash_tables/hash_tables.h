d36398b02df2a8f250d139a022df742dfb7dca9d
timontuitoek <tuitoektimon@gmail.com>
2023-09-07 10:35:20 -0700

    A function that adds an element to the hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -35,10 +35,10 @@ typedef struct hash_table_s
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+int hash_table_set(hash_table_t *htable, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
-void hash_table_print(const hash_table_t *ht);
-void hash_table_delete(hash_table_t *ht);
+void hash_table_print(const hash_table_t *htable);
+void hash_table_delete(hash_table_t *htable);
 
 /**
  * struct shash_node_s - Node of a sorted hash table
@@ -76,11 +76,11 @@ typedef struct shash_table_s
 } shash_table_t;
 
 shash_table_t *shash_table_create(unsigned long int size);
-int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-char *shash_table_get(const shash_table_t *ht, const char *key);
-void shash_table_print(const shash_table_t *ht);
-void shash_table_print_rev(const shash_table_t *ht);
-void shash_table_delete(shash_table_t *ht);
+int shash_table_set(shash_table_t *htable, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *htable, const char *key);
+void shash_table_print(const shash_table_t *htable);
+void shash_table_print_rev(const shash_table_t *htable);
+void shash_table_delete(shash_table_t *htable);
 
 #endif
 
