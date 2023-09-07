b2bf0065df5cf60ad6c365b7ba738bf3f216f167
timontuitoek <tuitoektimon@gmail.com>
2023-09-07 10:22:26 -0700

    A function that gives you the index of a key

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Retrieves the index at which a key/value
+ * pair should be stored in the hash table's array.
+ * @key: The key for which the index is calculated.
+ * @size: The size of the hash table's array.
+ *
+ * Return: The calculated index for the given key.
+ * Description: This function uses the djb2 hashing algorithm.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    /* Calculate the hash using the djb2 algorithm and then mod it by the array size. */
+    return (hash_djb2(key) % size);
+}
+
