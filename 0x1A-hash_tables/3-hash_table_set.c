a2d49f743381d7e884ffdc7889e90cf00eadce9d
timontuitoek <tuitoektimon@gmail.com>
2023-09-13 11:12:07 -0700

    A function that adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,49 +12,49 @@
  */
 int hash_table_set(hash_table_t *htable, const char *key, const char *value)
 {
-    unsigned long int index = 0;
-    hash_node_t *new_node = NULL;
-    hash_node_t *existing_node = NULL;
+	unsigned long int index = 0;
+	hash_node_t *new_node = NULL;
+	hash_node_t *existing_node = NULL;
 
-    if (strcmp(key, "") == 0 || !htable)
-        return (0);
-    
-    index = key_index((const unsigned char *)key, htable->size); /* Calculate the hash index. */
+	if (strcmp(key, "") == 0 || !htable)
+	return (0);
 
-    if (htable->array[index] != NULL) /* Handle collision. */
-    {
-        existing_node = htable->array[index];
-        while (existing_node)
-        {
-            if (strcmp(existing_node->key, key) == 0)
-            {
-                free(existing_node->value);
-                existing_node->value = strdup(value); /* Update the value. */
-                return (1);
-            }
-            existing_node = existing_node->next;
-        }
-        
-        new_node = malloc(sizeof(hash_node_t));
-        if (new_node == NULL)
-            return (0);
-        
-        new_node->key = strdup(key);
-        new_node->value = strdup(value);
-        new_node->next = htable->array[index]; /* Insert the new node at the beginning of the list. */
-    }
-    else
-    {
-        new_node = malloc(sizeof(hash_node_t));
-        if (new_node == NULL)
-            return (0);
-        
-        new_node->key = strdup(key);
-        new_node->value = strdup(value);
-        new_node->next = NULL;
-    }
+	index = key_index((const unsigned char *)key, htable->size); /* Calculate the hash index. */
 
-    htable->array[index] = new_node; /* Update the array at the calculated index. */
-    return (1);
+	if (htable->array[index] != NULL) /* Handle collision. */
+	{
+	existing_node = htable->array[index];
+	while (existing_node)
+	{
+	if (strcmp(existing_node->key, key) == 0)
+	{
+	free(existing_node->value);
+	existing_node->value = strdup(value); /* Update the value. */
+	return (1);
+	}
+	existing_node = existing_node->next;
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = htable->array[index]; /* Insert the new node at the beginning of the list. */
+	}
+	else
+	{
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+	}
+
+	htable->array[index] = new_node; /* Update the array at the calculated index. */
+	return (1);
 }
 
