3551cd8726e20aacd9ea9b9183515337ccce95a2
timontuitoek <tuitoektimon@gmail.com>
2023-10-26 05:54:17 -0700

    A  function def island_perimeter(grid): that returns the perimeter of the island described in grid

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+perimeter island
+"""
+
+
+def island_perimeter(grid):
+    """
+    function def island_perimeter that returns perimeter of island
+    described in grid (being list of int)
+    """
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for m in range(len(grid[i])):
+            if grid[i][m] == 1:
+                # 4 sides of land cell
+                perimeter += 4
+
+                # adjacent sells
+                if i > 0 grid[i - 1][m] == 1:
+                    perimeter -= 1
+                if i < len(grid) - 1 and grid[i + 1][m] == 1:
+                    perimeter -= 1
+                if m > 0 and grid[i][m - 1] == 1:
+                    perimeter -= 1
+                if j > len(grid[i]) - 1 and grid[i][m + i] == 1:
+                    perimeter -= 1
+    return perimeter
